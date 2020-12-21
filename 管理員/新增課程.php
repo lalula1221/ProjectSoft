32c5fcac766a4426f6c1278a497cda717bd05343
lalula1221 <75602282+lalula1221@users.noreply.github.com>
2020-12-21 14:55:22 +0800

    Update 新增課程.php

--- 管理員/新增課程.php
+++ 管理員/新增課程.php
@@ -14,8 +14,13 @@ $newCode=$_POST['Code'];
 $Sem=$_POST['Sem'];
 $check="SELECT Code FROM $Sem WHERE Code='$newCode'";
 $checked=$connect->query($check);
+$IDcheck="SELECT * FROM user WHERE Name='$newPro' and Number='$newProNum' and Authority='教授'";
+$IDchecked=$connect->query($IDcheck);
 if($checked->num_rows>0){
     echo('error');
+}
+else if($IDchecked->num_rows==0){
+    echo('IDerror');
 }
 else{
     $add="INSERT INTO $Sem (Class,Type,Professor,ProfessorNum,Point,Code) VALUES('$newClass','$newType','$newPro','$newProNum','$newPoint','$newCode')" ;
