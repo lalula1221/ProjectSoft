296ace29d8c0c6ff718d169e77c37ea99819c903
lalula1221 <75602282+lalula1221@users.noreply.github.com>
2020-12-27 22:31:40 +0800

    K

--- 管理員/修改課程.php
+++ 管理員/修改課程.php
@@ -8,16 +8,23 @@ $connect = new mysqli($host, $user, $passwd, $database);
 $oldCode=$_POST['oldCode'];
 $newClass=$_POST['Class'];
 $newType=$_POST['Type'];
-$newPro=$_POST['Professor'];
+$newProNum=$_POST['ProfessorNum'];
 $newPoint=$_POST['Point'];
 $Sem=$_POST['Sem'];
 $oldCheck="SELECT Code FROM $Sem WHERE Code='$oldCode'";
 $oldChecked=$connect->query($oldCheck);
+$IDcheck="SELECT Name,Number FROM user WHERE Number='$newProNum'";
+$IDchecked=$connect->query($IDcheck);
 if($oldChecked->num_rows==0){
     echo('error');
 }
+else if($IDchecked->num_rows==0){
+    echo('IDerror');
+}
 else{
-    $update="UPDATE $Sem SET Class='$newClass',Type='$newType',Professor='$newPro',Point='$newPoint' WHERE Code='$oldCode'";
+    $nameAndNum=$IDchecked->fetch_row();
+    $newPro=$nameAndNum[0];
+    $update="UPDATE $Sem SET Class='$newClass',Type='$newType',Professor='$newPro',ProfessorNum='$newProNum',Point='$newPoint' WHERE Code='$oldCode'";
     $connect->query($update);
 }
 ?>
